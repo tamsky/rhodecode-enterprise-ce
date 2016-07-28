9e6e897d0b936ac1cebd035751662f0a3751af71
Martin Bornhold <martin@rhodecode.com>
2016-07-28 09:54:35 +0200

    svn: Remove print statement and add __name__ to logger.

--- rhodecode/events/repo_group.py
+++ rhodecode/events/repo_group.py
@@ -22,7 +22,7 @@ from rhodecode.translation import lazy_ugettext
 from rhodecode.events.base import RhodecodeEvent
 
 
-log = logging.getLogger()
+log = logging.getLogger(__name__)
 
 
 class RepoGroupEvent(RhodecodeEvent):
