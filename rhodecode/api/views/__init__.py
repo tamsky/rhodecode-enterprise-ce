f54f46c73778ed426ff6e1a2acdaeae89ce75640
Marcin Kuzminski <marcin@rhodecode.com>
2017-01-02 09:30:39 +0100

    license: updated copyright year to 2017

--- rhodecode/api/views/__init__.py
+++ rhodecode/api/views/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2015-2016  RhodeCode GmbH
+# Copyright (C) 2015-2017 RhodeCode GmbH
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License, version 3
