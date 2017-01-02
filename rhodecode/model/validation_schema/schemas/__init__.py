924c666649003efac51ce7ff2168543fe69e9e73
Marcin Kuzminski <marcin@rhodecode.com>
2017-01-02 09:30:39 +0100

    license: updated copyright year to 2017

--- rhodecode/model/validation_schema/schemas/__init__.py
+++ rhodecode/model/validation_schema/schemas/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2016-2016  RhodeCode GmbH
+# Copyright (C) 2016-2017 RhodeCode GmbH
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License, version 3
