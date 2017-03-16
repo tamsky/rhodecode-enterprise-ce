6dc28e53aeabff9e04f4a5fb16419ca5f0942716
Marcin Kuzminski <marcin@rhodecode.com>
2017-03-16 15:06:17 +0100

    mercurial: enabled support of verify command.

--- rhodecode/lib/vcs/backends/hg/repository.py
+++ rhodecode/lib/vcs/backends/hg/repository.py
@@ -267,6 +267,12 @@ class MercurialRepository(BaseRepository):
         self.commit_ids = self._get_all_commit_ids()
         self._rebuild_cache(self.commit_ids)
 
+    def verify(self):
+        verify = self._remote.verify()
+
+        self._remote.invalidate_vcs_cache()
+        return verify
+
     def get_common_ancestor(self, commit_id1, commit_id2, repo2):
         if commit_id1 == commit_id2:
             return commit_id1
