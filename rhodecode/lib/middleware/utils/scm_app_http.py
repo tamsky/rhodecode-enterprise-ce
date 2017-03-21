c82b5a19c335428b28ac6b9808b99842bd5cd53d
Marcin Kuzminski <marcin@rhodecode.com>
2017-03-21 14:37:47 +0100

    git-lfs: streaming support for file upload.
    - ref #4235

--- rhodecode/lib/middleware/utils/scm_app_http.py
+++ rhodecode/lib/middleware/utils/scm_app_http.py
@@ -127,17 +127,32 @@ class VcsHttpProxy(object):
 
 
 def _maybe_stream_request(environ):
-    if environ.get('HTTP_TRANSFER_ENCODING', '') == 'chunked':
+    path = environ['PATH_INFO']
+    stream = _is_request_chunked(environ)
+    log.debug('handling request `%s` with stream support: %s', path, stream)
+
+    if stream:
         return environ['wsgi.input']
     else:
         return environ['wsgi.input'].read()
+
+
+def _is_request_chunked(environ):
+    stream = environ.get('HTTP_TRANSFER_ENCODING', '') == 'chunked'
+    if not stream:
+        # git lfs should stream for PUT requests which are upload
+        stream = ('git-lfs' in environ.get('HTTP_USER_AGENT', '')
+                  and environ['REQUEST_METHOD'] == 'PUT')
+    return stream
 
 
 def _maybe_stream_response(response):
     """
     Try to generate chunks from the response if it is chunked.
     """
-    if _is_chunked(response):
+    stream = _is_chunked(response)
+    log.debug('returning response with stream: %s', stream)
+    if stream:
         return response.raw.read_chunked()
     else:
         return [response.content]
