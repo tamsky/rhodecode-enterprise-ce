640d0931ae7657948f6f8a7db5e06a487da2b9d2
Marcin Kuzminski <marcin@rhodecode.com>
2017-10-24 23:48:14 +0200

    ssh-support: enabled full handling of all backends via SSH.
    - run callback deamons
    - run proper authentication checks
    - add relative loging.
    - fixes #5312

---
+++ rhodecode/apps/ssh_support/tests/conftest.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016-2017 RhodeCode GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3
+# (only), as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program is dual-licensed. If you wish to learn more about the
+# RhodeCode Enterprise Edition, including its added features, Support services,
+# and proprietary license terms, please see https://rhodecode.com/licenses/
+
+import os
+import pytest
+import ConfigParser
+
+from rhodecode.apps.ssh_support.lib.ssh_wrapper import SshWrapper
+from rhodecode.lib.utils2 import AttributeDict
+
+
+@pytest.fixture
+def dummy_conf_file(tmpdir):
+    conf = ConfigParser.ConfigParser()
+    conf.add_section('app:main')
+    conf.set('app:main', 'ssh.executable.hg', '/usr/bin/hg')
+    conf.set('app:main', 'ssh.executable.git', '/usr/bin/git')
+    conf.set('app:main', 'ssh.executable.svn', '/usr/bin/svnserve')
+
+    f_path = os.path.join(str(tmpdir), 'ssh_wrapper_test.ini')
+    with open(f_path, 'wb') as f:
+        conf.write(f)
+
+    return os.path.join(f_path)
+
+
+@pytest.fixture
+def dummy_env():
+    return {
+        'request':
+            AttributeDict(host_url='http://localhost', script_name='/')
+    }
+
+
+@pytest.fixture
+def dummy_user():
+    return AttributeDict(username='test_user')
+
+
+@pytest.fixture
+def ssh_wrapper(app, dummy_conf_file, dummy_env):
+    conn_info = '127.0.0.1 22 10.0.0.1 443'
+    return SshWrapper(
+        'random command', conn_info, 'auto', 'admin', '1', key_id='1',
+        shell=False, ini_path=dummy_conf_file, env=dummy_env)
--- rhodecode/apps/ssh_support/tests/test_server_git.py
+++ rhodecode/apps/ssh_support/tests/test_server_git.py
@@ -19,88 +19,100 @@
 # and proprietary license terms, please see https://rhodecode.com/licenses/
 
 import json
-
+import mock
 import pytest
-from mock import Mock, patch
 
 from rhodecode.apps.ssh_support.lib.backends.git import GitServer
-
-
-@pytest.fixture
-def git_server():
-    return GitServerCreator()
+from rhodecode.apps.ssh_support.tests.conftest import dummy_env, dummy_user
 
 
 class GitServerCreator(object):
     root = '/tmp/repo/path/'
-    git_path = '/usr/local/bin/'
+    git_path = '/usr/local/bin/git'
     config_data = {
         'app:main': {
-            'ssh.executable.git': git_path
+            'ssh.executable.git': git_path,
+            'vcs.hooks.protocol': 'http',
         }
     }
     repo_name = 'test_git'
     repo_mode = 'receive-pack'
-    user = 'vcs'
+    user = dummy_user()
 
     def __init__(self):
         def config_get(part, key):
             return self.config_data.get(part, {}).get(key)
-        self.config_mock = Mock()
-        self.config_mock.get = Mock(side_effect=config_get)
+        self.config_mock = mock.Mock()
+        self.config_mock.get = mock.Mock(side_effect=config_get)
 
     def create(self, **kwargs):
         parameters = {
-            'store': {'path': self.root},
+            'store': self.root,
             'ini_path': '',
             'user': self.user,
             'repo_name': self.repo_name,
             'repo_mode': self.repo_mode,
             'user_permissions': {
-                self.repo_name: 'repo_admin'
+                self.repo_name: 'repository.admin'
             },
             'config': self.config_mock,
+            'env': dummy_env()
         }
         parameters.update(kwargs)
         server = GitServer(**parameters)
         return server
 
 
+@pytest.fixture
+def git_server(app):
+    return GitServerCreator()
+
+
 class TestGitServer(object):
+
     def test_command(self, git_server):
         server = git_server.create()
-        server.read_only = False
         expected_command = (
-            'cd {root}; {git_path}-{repo_mode}'
-            ' \'{root}{repo_name}\''.format(
+            'cd {root}; {git_path} {repo_mode} \'{root}{repo_name}\''.format(
                 root=git_server.root, git_path=git_server.git_path,
                 repo_mode=git_server.repo_mode, repo_name=git_server.repo_name)
         )
-        assert expected_command == server.command
+        assert expected_command == server.tunnel.command()
 
-    def test_run_returns_exit_code_2_when_no_permissions(self, git_server, caplog):
+    @pytest.mark.parametrize('permissions, action, code', [
+        ({}, 'pull', -2),
+        ({'test_git': 'repository.read'}, 'pull', 0),
+        ({'test_git': 'repository.read'}, 'push', -2),
+        ({'test_git': 'repository.write'}, 'push', 0),
+        ({'test_git': 'repository.admin'}, 'push', 0),
+
+    ])
+    def test_permission_checks(self, git_server, permissions, action, code):
+        server = git_server.create(user_permissions=permissions)
+        result = server._check_permissions(action)
+        assert result is code
+
+    @pytest.mark.parametrize('permissions, value', [
+        ({}, False),
+        ({'test_git': 'repository.read'}, False),
+        ({'test_git': 'repository.write'}, True),
+        ({'test_git': 'repository.admin'}, True),
+
+    ])
+    def test_has_write_permissions(self, git_server, permissions, value):
+        server = git_server.create(user_permissions=permissions)
+        result = server.has_write_perm()
+        assert result is value
+
+    def test_run_returns_executes_command(self, git_server):
         server = git_server.create()
-        with patch.object(server, '_check_permissions') as permissions_mock:
-            with patch.object(server, '_update_environment'):
-                permissions_mock.return_value = 2
+        from rhodecode.apps.ssh_support.lib.backends.git import GitTunnelWrapper
+        with mock.patch.object(GitTunnelWrapper, 'create_hooks_env') as _patch:
+            _patch.return_value = 0
+            with mock.patch.object(GitTunnelWrapper, 'command', return_value='date'):
                 exit_code = server.run()
 
-        assert exit_code == (2, False)
-
-    def test_run_returns_executes_command(self, git_server, caplog):
-        server = git_server.create()
-        with patch.object(server, '_check_permissions') as permissions_mock:
-            with patch('os.system') as system_mock:
-                with patch.object(server, '_update_environment') as (
-                        update_mock):
-                    permissions_mock.return_value = 0
-                    system_mock.return_value = 0
-                    exit_code = server.run()
-
-        system_mock.assert_called_once_with(server.command)
-        update_mock.assert_called_once_with()
-
-        assert exit_code == (0, True)
+        assert exit_code == (0, False)
 
     @pytest.mark.parametrize(
         'repo_mode, action', [
@@ -109,88 +121,25 @@ class TestGitServer(object):
         ])
     def test_update_environment(self, git_server, repo_mode, action):
         server = git_server.create(repo_mode=repo_mode)
-        with patch('os.environ', {'SSH_CLIENT': '10.10.10.10 b'}):
-            with patch('os.putenv') as putenv_mock:
-                server._update_environment()
+        with mock.patch('os.environ', {'SSH_CLIENT': '10.10.10.10 b'}):
+            with mock.patch('os.putenv') as putenv_mock:
+                server.update_environment(action)
 
         expected_data = {
-            "username": git_server.user,
-            "scm": "git",
-            "repository": git_server.repo_name,
-            "make_lock": None,
-            "action": [action],
-            "ip": "10.10.10.10",
-            "locked_by": [None, None],
-            "config": ""
+            'username': git_server.user.username,
+            'scm': 'git',
+            'repository': git_server.repo_name,
+            'make_lock': None,
+            'action': action,
+            'ip': '10.10.10.10',
+            'locked_by': [None, None],
+            'config': '',
+            'server_url': None,
+            'hooks': ['push', 'pull'],
+            'is_shadow_repo': False,
+            'hooks_module': 'rhodecode.lib.hooks_daemon',
+            'SSH': True,
+            'SSH_PERMISSIONS': 'repository.admin',
         }
         args, kwargs = putenv_mock.call_args
         assert json.loads(args[1]) == expected_data
-
-
-class TestGitServerCheckPermissions(object):
-    def test_returns_2_when_no_permissions_found(self, git_server, caplog):
-        user_permissions = {}
-        server = git_server.create(user_permissions=user_permissions)
-        result = server._check_permissions()
-        assert result == 2
-
-        log_msg = 'permission for vcs on test_git are: None'
-        assert log_msg in [t[2] for t in caplog.record_tuples]
-
-    def test_returns_2_when_no_permissions(self, git_server, caplog):
-        user_permissions = {git_server.repo_name: 'repository.none'}
-        server = git_server.create(user_permissions=user_permissions)
-        result = server._check_permissions()
-        assert result == 2
-
-        log_msg = 'repo not found or no permissions'
-        assert log_msg in [t[2] for t in caplog.record_tuples]
-
-    @pytest.mark.parametrize(
-        'permission', ['repository.admin', 'repository.write'])
-    def test_access_allowed_when_user_has_write_permissions(
-            self, git_server, permission, caplog):
-        user_permissions = {git_server.repo_name: permission}
-        server = git_server.create(user_permissions=user_permissions)
-        result = server._check_permissions()
-        assert result is None
-
-        log_msg = 'Write Permissions for User "%s" granted to repo "%s"!' % (
-            git_server.user, git_server.repo_name)
-        assert log_msg in [t[2] for t in caplog.record_tuples]
-
-    def test_write_access_is_not_allowed_when_user_has_read_permission(
-            self, git_server, caplog):
-        user_permissions = {git_server.repo_name: 'repository.read'}
-        server = git_server.create(
-            user_permissions=user_permissions, repo_mode='receive-pack')
-        result = server._check_permissions()
-        assert result == -3
-
-        log_msg = 'Only Read Only access for User "%s" granted to repo "%s"! Failing!' % (
-            git_server.user, git_server.repo_name)
-        assert log_msg in [t[2] for t in caplog.record_tuples]
-
-    def test_read_access_allowed_when_user_has_read_permission(
-            self, git_server, caplog):
-        user_permissions = {git_server.repo_name: 'repository.read'}
-        server = git_server.create(
-            user_permissions=user_permissions, repo_mode='upload-pack')
-        result = server._check_permissions()
-        assert result is None
-
-        log_msg = 'Only Read Only access for User "%s" granted to repo "%s"!' % (
-            git_server.user, git_server.repo_name)
-        assert log_msg in [t[2] for t in caplog.record_tuples]
-
-    def test_returns_error_when_permission_not_recognised(
-            self, git_server, caplog):
-        user_permissions = {git_server.repo_name: 'repository.whatever'}
-        server = git_server.create(
-            user_permissions=user_permissions, repo_mode='upload-pack')
-        result = server._check_permissions()
-        assert result == -2
-
-        log_msg = 'Cannot properly fetch user permission. ' \
-                  'Return value is: repository.whatever'
-        assert log_msg in [t[2] for t in caplog.record_tuples]
