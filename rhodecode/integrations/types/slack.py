4c65b972ca6bdd15239581d8269d7c9fbc1e54fc
Marcin Kuzminski <marcin@rhodecode.com>
2017-11-09 19:03:12 +0100

    slack: expose the FULL message instead of title.
    Because slack uses it's own trim, we should avoid sending trimmed data and let users see the whole set.

--- rhodecode/integrations/types/slack.py
+++ rhodecode/integrations/types/slack.py
@@ -265,7 +265,7 @@ class SlackIntegrationType(IntegrationTypeBase):
         %for branch, branch_commits in branches_commits.items():
         ${len(branch_commits['commits'])} ${'commit' if len(branch_commits['commits']) == 1 else 'commits'} on branch: <${branch_commits['branch']['url']}|${branch_commits['branch']['name']}>
         %for commit in branch_commits['commits']:
-        `<${commit['url']}|${commit['short_id']}>` - ${commit['message_html_title']|html_to_slack_links}
+        `<${commit['url']}|${commit['short_id']}>` - ${commit['message_html']|html_to_slack_links}
         %endfor
         %endfor
         '''))
