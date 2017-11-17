f094c9fe431b0d5f8026a7cd1e80c72907dcb44c
Marcin Kuzminski <marcin@rhodecode.com>
2017-11-17 15:57:51 +0100

    pytest: moved baseapp to plugin

--- rhodecode/tests/plugin.py
+++ rhodecode/tests/plugin.py
@@ -36,6 +36,7 @@ import pyramid.testing
 import pytest
 import colander
 import requests
+import pyramid.paster
 
 import rhodecode
 from rhodecode.lib.utils2 import AttributeDict
@@ -216,6 +217,20 @@ def http_environ(http_host_stub):
         'HTTP_USER_AGENT': 'rc-test-agent',
         'REQUEST_METHOD': 'GET'
     }
+
+
+@pytest.fixture(scope='session')
+def baseapp(ini_config, vcsserver, http_environ_session):
+    from rhodecode.lib.pyramid_utils import get_app_config
+    from rhodecode.config.middleware import make_pyramid_app
+
+    print("Using the RhodeCode configuration:{}".format(ini_config))
+    pyramid.paster.setup_logging(ini_config)
+
+    settings = get_app_config(ini_config)
+    app = make_pyramid_app({'__file__': ini_config}, **settings)
+
+    return app
 
 
 @pytest.fixture(scope='function')
