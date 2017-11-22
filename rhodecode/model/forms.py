233b248b6e0a62d154287bb6e644f8efee68aba3
Marcin Kuzminski <marcin@rhodecode.com>
2017-11-22 20:59:25 +0100

    emails: fixed validation of emails with whitespace in them.
    - fixes problem with potential duplicate emails addresses for users.

--- rhodecode/model/forms.py
+++ rhodecode/model/forms.py
@@ -137,7 +137,7 @@ def UserForm(edit=False, available_languages=[], old_data={}):
         active = v.StringBoolean(if_missing=False)
         firstname = v.UnicodeString(strip=True, min=1, not_empty=False)
         lastname = v.UnicodeString(strip=True, min=1, not_empty=False)
-        email = All(v.Email(not_empty=True), v.UniqSystemEmail(old_data))
+        email = All(v.UniqSystemEmail(old_data), v.Email(not_empty=True))
         extern_name = v.UnicodeString(strip=True)
         extern_type = v.UnicodeString(strip=True)
         language = v.OneOf(available_languages, hideList=False,
@@ -219,7 +219,7 @@ def RegisterForm(edit=False, old_data={}):
         active = v.StringBoolean(if_missing=False)
         firstname = v.UnicodeString(strip=True, min=1, not_empty=False)
         lastname = v.UnicodeString(strip=True, min=1, not_empty=False)
-        email = All(v.Email(not_empty=True), v.UniqSystemEmail(old_data))
+        email = All(v.UniqSystemEmail(old_data), v.Email(not_empty=True))
 
         chained_validators = [v.ValidPasswordsMatch()]
 
