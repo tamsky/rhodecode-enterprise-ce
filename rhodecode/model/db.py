37d2cc8b37804667c3b3173c3dd47dec14f046b5
Marcin Kuzminski <marcin@rhodecode.com>
2019-01-18 17:21:29 +0100

    remap-rescan: prevent empty/damaged repos to break the remap operation.

--- rhodecode/model/db.py
+++ rhodecode/model/db.py
@@ -2276,7 +2276,7 @@ class Repository(Base, BaseModel):
             # use no-cache version here
             scm_repo = self.scm_instance(cache=False, config=config)
 
-            empty = scm_repo.is_empty()
+            empty = not scm_repo or scm_repo.is_empty()
             if not empty:
                 cs_cache = scm_repo.get_commit(
                     pre_load=["author", "date", "message", "parents"])
