1230b2cf528c0ed9f1b6da42bcf7e6c57af91096
Peter Colledge <pcolledge@solarflare.com>
2019-03-26 14:52:34 +0000

    Issue #5548 - Add repo_group_name and docs to search API

--- rhodecode/api/views/search_api.py
+++ rhodecode/api/views/search_api.py
@@ -32,7 +32,7 @@ log = logging.getLogger(__name__)
 
 
 @jsonrpc_method()
-def search(request, apiuser, search_query, search_type, page_limit=Optional(10), page=Optional(1), search_sort=Optional('newfirst'), repo_name=Optional(None)):
+def search(request, apiuser, search_query, search_type, page_limit=Optional(10), page=Optional(1), search_sort=Optional('newfirst'), repo_name=Optional(None), repo_group_name=Optional(None)):
     """
     Search
 
@@ -40,21 +40,29 @@ def search(request, apiuser, search_query, search_type, page_limit=Optional(10),
     :type apiuser: AuthUser
     :param search_query: Search query.
     :type search_query: str
-    :param search_type: Search type. either commit or content.
+    :param search_type: Search type. The following are valid options:
+        * commit
+        * content
+        * path
     :type search_type: str
-    :param page_limit: Page item limit. Default 10 items.
+    :param page_limit: Page item limit, from 1 to 500. Default 10 items.
     :type page_limit: Optional(int)
     :param page: Page number. Default first page.
     :type page: Optional(int)
-    :param search_sort: Search sort. Default newfirst.
+    :param search_sort: Search sort order. Default newfirst. The following are valid options:
+        * newfirst
+        * oldfirst
     :type search_sort: Optional(str)
     :param repo_name: Filter by one repo. Default is all.
     :type repo_name: Optional(str)
+    :param repo_group_name: Filter by one repo group. Default is all.
+    :type repo_group_name: Optional(str)
     """
 
     searcher = searcher_from_config(request.registry.settings)
     data = {'execution_time': ''}
     repo_name = Optional.extract(repo_name)
+    repo_group_name = Optional.extract(repo_group_name)
 
     schema = search_schema.SearchParamsSchema()
 
